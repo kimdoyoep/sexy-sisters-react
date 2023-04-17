a3e2a6a4d845e6a7fd6138733304060a0915cbd2
Kimdoyeop <dpdp39000@gmail.com>
2023-04-17 21:57:21 +0900

    Study React 5.12?

--- src/routes/chart.tsx
+++ src/routes/chart.tsx
@@ -1,4 +1,12 @@
-export default function Chart() {
+import { useQuery } from 'react-query';
+import { fetchCoinHistory } from './Api';
+
+interface ChartProps {
+  coinId : string;
+}
+
+export default function Chart({coinId} : ChartProps) {
+  const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId))
   return(
     <>Chart</>
   )
