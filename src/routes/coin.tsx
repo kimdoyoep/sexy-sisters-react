e139c131f53fa8a6e0eb13c58187b897babb251b
Kimdoyeop <dpdp39000@gmail.com>
2023-04-17 14:32:20 +0900

    Study React 5.6

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -25,6 +25,30 @@ const Header = styled.header`
   align-items : center;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+  color: black;
+  margin-top: 30px
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 
 
 export default function Coin() {
@@ -100,14 +124,15 @@ export default function Coin() {
 
   const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<InfoData>();
-  const [infoPrice, setInfoPrice] = useState<PriceData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
 
 useEffect(() => {
   (async () => {
     const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
     const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
     setInfo(infoData);
-    setInfoPrice(priceData);
+    setPriceInfo(priceData);
+    setLoading(false);
   })();
 }, [])
 
@@ -118,7 +143,37 @@ useEffect(() => {
       <Title>{state?.name || null}</Title>
     </Header>
 
-    {loading ? (<Loader>Loading...</Loader>) : null }
+    {loading ? (<Loader>Loading...</Loader>) : 
+    
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+
+    }
     </Container>
   )
 }
