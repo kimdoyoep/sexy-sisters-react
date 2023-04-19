b3f3138d755ba5271f6a4a00e95076f084c79306
Kimdoyeop <dpdp39000@gmail.com>
2023-04-19 11:56:04 +0900

    Study React 6.6

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,23 +1,26 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function ToDoList() {
 
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  }; 
+  // const [toDo, setToDo] = useState("");
+  // const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+  //   const {
+  //     currentTarget: { value },
+  //   } = event;
+  //   setToDo(value);
+  // };
+  // const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   console.log(toDo);
+  // }; 
 
+  const {register, watch} = useForm();
+  console.log(watch())
   return(
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder='Write a to do'/>
+      <form>
+        <input {...register("Email")} placeholder='Email'/>
         <button>Add</button>
       </form>
     </div>
