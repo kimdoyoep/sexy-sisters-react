573c0f6c3719a0b1d8398afc9b397b7ddfcae9f9
Kimdoyeop <dpdp39000@gmail.com>
2023-04-20 09:51:37 +0900

    Study React 6.16

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,12 +1,13 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { toDoState } from './Atom';
+import { selector, useRecoilState, useRecoilValue } from 'recoil';
+import { toDoSelector, toDoState } from './Atom';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 
 export default function ToDoList() {
 
-  const toDo = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+
   return (
     <div>
 
@@ -15,11 +16,29 @@ export default function ToDoList() {
 
       <CreateToDo></CreateToDo>
 
+      <h2>To Do</h2>
       <ul>
         {toDo.map(toDo => (
             <ToDo key={toDo.id} {...toDo}></ToDo>
         ))}
       </ul>
+      <hr></hr>
+
+      <h2>Doing</h2>
+      <ul>
+        {doing.map(doing => (
+            <ToDo key={doing.id} {...doing}></ToDo>
+        ))}
+      </ul>
+      <hr></hr>
+
+      <h2>Done</h2>
+      <ul>
+        {done.map(done => (
+            <ToDo key={done.id} {...done}></ToDo>
+        ))}
+      </ul>
+      <hr></hr>
 
     </div>
   )
